5227762b7e2dd0ac936fa31d9180b8cba99edd7e
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-02-27 00:46:57 +0530

    fixing responsiveness

--- src/components/CarouselDeals.jsx
+++ src/components/CarouselDeals.jsx
@@ -27,6 +27,32 @@ const settings = {
   slidesToShow: 5,
   slidesToScroll: 2,
   initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 export default function CarouselDeals() {
